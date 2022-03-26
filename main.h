ca6e450439d6c540b482af0790a9c2da9ef2aeed
bcondict <jesus.junc10@gmail.com>
2022-03-26 15:30:24 -0500

    betty Readme file corrections Jesus

--- main.h
+++ main.h
@@ -12,7 +12,7 @@ int ini_printf(const char *format, va_list args);
 int _printf(const char *format, ...);
 int _putchar(char c);
 void option_c(const char *format, va_list args);
-void testing(char * string);
+void testing(char *string);
 int option_s(const char *format, va_list args);
 
 #endif
